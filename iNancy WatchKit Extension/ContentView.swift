30bca390d1652b012e9c4386004ecff62c1236b6
Massimo <massimo.maddaluno97@gmail.com>
2020-01-17 14:02:27 +0100

    initial commit

---
+++ iNancy WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iNancy WatchKit Extension
+//
+//  Created by Massimo Maddaluno on 17/01/2020.
+//  Copyright © 2020 Massimo Maddaluno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iNancy WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  iNancy WatchKit Extension
+//
+//  Created by Massimo Maddaluno on 17/01/2020.
+//  Copyright © 2020 Massimo Maddaluno. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
